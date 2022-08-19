7ce3bff293ed948c97e1c9fa0ed49e36d00d3a96
b4ljk <baljinnyam.dayan@gmail.com>
2022-08-19 17:56:54 +0800

    home is functional

--- src/home/index.js
+++ src/home/index.js
@@ -1,5 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import {SafeAreaView, View, Text, Image, TouchableOpacity} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
 import {styles} from '../assets/styles';
 import {useAuth} from '../settings/authContext';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -8,46 +15,87 @@ import {color} from 'react-native-reanimated';
 
 const HomeScreen = ({navigation}) => {
   const {signOutUser} = useAuth();
-
-  const RenderEachChat = second => {
+  const [data, setData] = useState([
+    {
+      id: 1,
+      userName: 'Dorjo',
+      avatar: require('../assets/img/town.png'),
+      lastChat: 'Balduran come back',
+      time: '7:55',
+    },
+    {
+      id: 2,
+      userName: 'Ugandan prime minister bataa',
+      avatar: require('../assets/img/town.png'),
+      lastChat: 'Balduran come back',
+      time: '17:55',
+    },
+    {
+      id: 3,
+      userName: 'Jesus',
+      lastChat: 'Balduran come back',
+      avatar: require('../assets/img/town.png'),
+      time: '1:55',
+    },
+    {
+      id: 4,
+      userName: 'Mathew',
+      lastChat: 'Balduran come back',
+      avatar: require('../assets/img/town.png'),
+      time: '17:595',
+    },
+  ]);
+  const RenderEachChat = ({item}) => {
     return (
-      <View
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('Chat');
+        }}
         style={{
-          height: 90,
+          marginVertical: 2,
           borderRadius: 20,
           backgroundColor: colors.gray100,
-          alignItems: 'center',
-          flexDirection: 'row',
-          marginVertical: 2,
         }}>
-        <Image
-          style={{height: 70, width: 70, borderRadius: 70, marginLeft: 10}}
-          resizeMode="contain"
-          source={{
-            uri: 'https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png',
-          }}
-        />
-        <View>
-          <View
-            style={{
-              justifyContent: 'space-evenly',
+        <View
+          style={{
+            height: 90,
+            borderRadius: 20,
 
-              flex: 1,
-              marginVertical: 20,
-              marginLeft: 10,
-            }}>
-            <Text style={{fontFamily: 'Roboto-Bold', fontSize: 18}}>
-              Baldan
-            </Text>
-            <Text style={{fontFamily: 'Roboto-Bold', fontSize: 15}}>
-              You: Balduran come home
-            </Text>
+            alignItems: 'center',
+            flexDirection: 'row',
+          }}>
+          <Image
+            style={{height: 70, width: 70, borderRadius: 70, marginLeft: 10}}
+            resizeMode="contain"
+            source={{
+              uri: 'https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png',
+            }}
+          />
+          <View style={{flex: 4}}>
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'space-evenly',
+                marginVertical: 17,
+                marginLeft: 10,
+              }}>
+              <Text
+                numberOfLines={1}
+                style={{fontFamily: 'Roboto-Bold', fontSize: 18}}>
+                {item.userName}
+              </Text>
+              <Text
+                numberOfLines={1}
+                style={{fontFamily: 'Roboto-Bold', fontSize: 15}}>
+                You: Balduran come home
+              </Text>
+            </View>
+          </View>
+          <View style={{flex: 1, marginRight: 10}}>
+            <Text style={{alignSelf: 'flex-end'}}>{item.time}</Text>
           </View>
         </View>
-        <View style={{flex: 1, marginRight: 10}}>
-          <Text style={{alignSelf: 'flex-end'}}>04:25</Text>
-        </View>
-      </View>
+      </TouchableOpacity>
     );
   };
   return (
@@ -60,11 +108,13 @@ const HomeScreen = ({navigation}) => {
               <Icon name="pencil-square-o" size={25} />
             </View>
           </View>
-          <View style={styles.body}></View>
-          <RenderEachChat />
-          <RenderEachChat />
-          <RenderEachChat />
-          <RenderEachChat />
+          <View style={styles.body}>
+            <FlatList
+              style={[styles.body, {paddingVertical: 10}]}
+              data={data}
+              renderItem={RenderEachChat}
+            />
+          </View>
         </View>
       </SafeAreaView>
       <View
