7df33412e841d04bc861089440e8af22c611f112
b4ljk <baljinnyam.dayan@gmail.com>
2022-08-26 11:51:36 +0800

    chat

--- src/chat/index.js
+++ src/chat/index.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {SafeAreaView, View, Text, TextInput, TouchableOpacity} from 'react-native';
+import {SafeAreaView, View, Text, TextInput, TouchableOpacity, FlatList} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {colors} from '../assets/colors';
@@ -8,6 +8,59 @@ import * as Animatable from 'react-native-animatable';
 
 const ChatScreen = ({navigation, route}) => {
     const {responderName} = route.params;
+    const [chatData, setChatData] = useState([
+        {
+            id: 1,
+            message: 'Hello',
+            isUser: true,
+        },
+        {
+            id: 2,
+            message: 'Hello',
+            isUser: false,
+        },
+        {
+            id: 3,
+            message: 'zailaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+            isUser: true,
+        },
+        {
+            id: 4,
+            message: 'ok',
+            isUser: false,
+        },
+    ]);
+
+    const chatRenderItem = ({item}) => {
+        return item.isUser ? (
+            <View
+                style={{
+                    backgroundColor: colors.blue300,
+                    margin: 2.5,
+                    borderRadius: 12,
+                    paddingHorizontal: 8,
+                    paddingVertical: 4,
+                    maxWidth: '75%',
+                    alignSelf: 'flex-end',
+                }}>
+                <Text>{item.message}</Text>
+            </View>
+        ) : (
+            <View
+                style={{
+                    backgroundColor: colors.blue100,
+                    margin: 2.5,
+                    borderRadius: 12,
+                    paddingHorizontal: 8,
+                    paddingVertical: 4,
+                    maxWidth: '75%',
+                    alignSelf: 'flex-start',
+                }}>
+                <Text>{item.message}</Text>
+            </View>
+        );
+    };
+
     return (
         <SafeAreaView style={styles.body}>
             <View style={[styles.row, styles.center, {justifyContent: 'flex-start', padding: 10}]}>
@@ -20,7 +73,13 @@ const ChatScreen = ({navigation, route}) => {
                 </TouchableOpacity>
                 <Text>{responderName}</Text>
             </View>
-            <View style={styles.body}></View>
+            <View style={styles.body}>
+                <FlatList
+                    data={chatData}
+                    keyExtractor={item => item.id.toString()}
+                    renderItem={chatRenderItem}
+                />
+            </View>
             <View style={[styles.row, {alignItems: 'flex-end', marginBottom: 6}]}>
                 <TouchableOpacity style={{paddingHorizontal: 6}}>
                     <Ionicons name="ios-camera" size={25} />
